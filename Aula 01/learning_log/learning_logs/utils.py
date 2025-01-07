27c0faf91e42cb3c78f30e3e4de7b3bc05dd7ee5
Casasco <gabrielcasasco2@gmail.com>
2025-01-07 17:53:50 -0300

    Projeto Funcional

--- Aula 01/learning_log/learning_logs/utils.py
+++ Aula 01/learning_log/learning_logs/utils.py
@@ -3,6 +3,7 @@ from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 import logging
+from colorama import Fore, Style
 
 # Configuração do logging
 logging.basicConfig(filename='drive_api.log', level=logging.ERROR)
@@ -75,11 +76,117 @@ def listar_pastas_drive():
     return folders  # Retorna a lista de pastas
 
 
+def upload_pdf_to_folders(file_path, folder_ids):
+    from googleapiclient.discovery import build
+    from googleapiclient.http import MediaFileUpload
+    from google.oauth2.service_account import Credentials
+    import os
+    from colorama import Fore, Style
 
+    SCOPES = ['https://www.googleapis.com/auth/drive']
+    credentials = Credentials.from_service_account_file(
+        r"C:\Users\Geral\Desktop\Curso de Django\Aula 01\learning_log\credentials\uploadcontratos-442920-4a331bd12026.json",
+        scopes=SCOPES
+    )
+    service = build('drive', 'v3', credentials=credentials)
 
-        
+    results = {}
+    for folder_id in folder_ids:
+        try:
+            # Obter o nome da pasta pai
+            folder_metadata = service.files().get(
+                fileId=folder_id,
+                fields="name",
+                supportsAllDrives=True
+            ).execute()
+            parent_folder_name = folder_metadata.get('name')
+            if "- (" in parent_folder_name:
+                parent_folder_name = parent_folder_name.split(" - (")[0].strip()
+            else:
+                parent_folder_name = parent_folder_name.split(" - ANCINE")[0].strip()
+            parent_folder_name = parent_folder_name.title()
 
+            print(Fore.CYAN + f"Nome da pasta pai: {parent_folder_name}" + Style.RESET_ALL)
 
-  
+            # Busca subpastas na pasta pai
+            query_main = f"'{folder_id}' in parents and mimeType='application/vnd.google-apps.folder' and trashed=false"
+            subpastas = []
+            page_token_main = None
 
+            while True:
+                response_main = service.files().list(
+                    q=query_main,
+                    spaces='drive',
+                    fields='nextPageToken, files(id, name)',
+                    supportsAllDrives=True,
+                    includeItemsFromAllDrives=True,
+                    pageToken=page_token_main
+                ).execute()
 
+                for subfolder in response_main.get('files', []):
+                    subpastas.append({'id': subfolder['id'], 'name': str(subfolder['name']).lower()})
+                    print(Fore.YELLOW + f"Nome da subpasta: {subfolder['name']}" + Style.RESET_ALL)
+                    print(Fore.YELLOW + f"ID da subpasta: {subfolder['id']}" + Style.RESET_ALL)
+
+                    # Se encontra "7. Contratos" ou "5. Contratos", busca subpastas dentro dela
+                    if str(subfolder['name']).lower() in ["7. contratos", "5. contratos"]:
+                        print(Fore.MAGENTA + "Pasta 'Contratos' encontrada" + Style.RESET_ALL)
+                        id_PastaContratos = subfolder['id']
+                        query_sub = f"'{id_PastaContratos}' in parents and mimeType='application/vnd.google-apps.folder' and trashed=false"
+                        page_token_sub = None
+
+                        while True:
+                            response_sub = service.files().list(
+                                q=query_sub,
+                                spaces='drive',
+                                fields='nextPageToken, files(id, name)',
+                                supportsAllDrives=True,
+                                includeItemsFromAllDrives=True,
+                                pageToken=page_token_sub
+                            ).execute()
+
+                            for subfolder3 in response_sub.get('files', []):
+                                print(Fore.YELLOW + f"Nome da subpasta: {subfolder3['name']}" + Style.RESET_ALL)
+                                print(Fore.YELLOW + f"ID da subpasta: {subfolder3['id']}" + Style.RESET_ALL)
+                                if str(subfolder3['name']).lower() == "c. contratos de exibição":
+                                    print(Fore.GREEN + "Pasta 'c. Contratos de Exibição' encontrada" + Style.RESET_ALL)
+                                    # Salvar o ID da pasta encontrada
+                                    id_PastaContratosExib = subfolder3['id']
+
+                                    # Nome personalizado: Nome do arquivo + Nome da pasta pai
+                                    base_name = os.path.basename(file_path).replace("_", " ").replace(".pdf", "")  # Substituir underscores e remover extensão
+                                    new_file_name = f"{base_name} - {parent_folder_name}.pdf"  # Adicionar extensão .pdf no final
+
+                                    # Metadados do arquivo
+                                    file_metadata = {
+                                        'name': new_file_name,
+                                        'parents': [id_PastaContratosExib]
+                                    }
+
+                                    # Enviar arquivo
+                                    media = MediaFileUpload(file_path, mimetype='application/pdf')
+                                    uploaded_file = service.files().create(
+                                        body=file_metadata,
+                                        media_body=media,
+                                        fields='id, name, parents',
+                                        supportsAllDrives=True  # Suporte para Drives compartilhados
+                                    ).execute()
+
+                                    results[folder_id] = Fore.GREEN + f"Arquivo enviado com sucesso: {uploaded_file['name']} (ID: {uploaded_file['id']})" + Style.RESET_ALL
+                                    print(Fore.GREEN + "Contrato subido na pasta 'c. Contratos de Exibição'" + Style.RESET_ALL)
+                                    break  # Interrompe a busca após encontrar e enviar o arquivo
+                                else:
+                                    print(Fore.MAGENTA + "Pasta 'c. Contratos de Exibição' não encontrada" + Style.RESET_ALL)
+
+                            page_token_sub = response_sub.get('nextPageToken', None)
+                            if page_token_sub is None:
+                                break
+
+                page_token_main = response_main.get('nextPageToken', None)
+                if page_token_main is None:
+                    break
+
+        except Exception as e:
+            results[folder_id] = Fore.RED + f"Erro: Não foi possível enviar o arquivo para a pasta '{folder_id}'. Detalhes: {e}" + Style.RESET_ALL
+
+    return results
--- Aula 01/learning_log/learning_logs/views.py
+++ Aula 01/learning_log/learning_logs/views.py
@@ -2,10 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Projeto 
 from .utils import listar_pastas_drive
+from .utils import upload_pdf_to_folders
 import os
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.utils.text import slugify
+from colorama import Fore, Style
 
 # Create your views here.
 
@@ -34,37 +38,55 @@ def salvar_ids(request):
     else:
         return HttpResponse("Método inválido.", status=400)
     
+import os
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render
+
 def subir_arquivo(request):
     """View para chamar método que sobe os arquivos para o drive"""
     if request.method == 'POST':
         id_pasta = request.POST.getlist('id_array')
-        array_id = []
-        for id in id_pasta:
-            # Adiciona o ID entre aspas duplas
-            array_id.append(f'"{id}"')
+        array_id = [id.strip('"') for id in id_pasta]
+        print(Fore.YELLOW + "IDs das pastas processados para upload:" + Style.RESET_ALL, array_id)
 
-        # Exemplo de debug: Imprimir o resultado para ver os IDs processados
-        print(array_id)
+        # Debug: Exibir IDs processados
+        print(Fore.YELLOW + "IDs das pastas recebidos:" + Style.RESET_ALL, array_id)
 
-         # Receber o arquivo selecionado
+        # Receber o arquivo selecionado
         uploaded_file = request.FILES.get('uploaded_file')
         if uploaded_file:
             # Salvar o arquivo no sistema de arquivos local temporariamente
-            fs = FileSystemStorage(location=os.path.join(settings.MEDIA_ROOT, 'uploads'))
-            file_path = fs.save(uploaded_file.name, uploaded_file)
-            file_url = fs.url(file_path)
+            temp_dir = r"C:\Temp"
+            if not os.path.exists(temp_dir):
+                os.makedirs(temp_dir)
 
-            # Debug: Exibir informações do arquivo e IDs
-            print(f"Arquivo enviado: {uploaded_file.name}")
-            print(f"IDs das pastas: {array_id}")
+            file_path = os.path.join(temp_dir, uploaded_file.name.replace(" ", "_"))
 
-            # Aqui você pode adicionar a lógica para manipular o arquivo no Google Drive
-            # ou outro destino necessário.
+            try:
+                # Salvar o arquivo localmente
+                with open(file_path, 'wb') as f:
+                    for chunk in uploaded_file.chunks():
+                        f.write(chunk)
+
+                # Chamar a função para fazer o upload do arquivo para o Google Drive
+                results = upload_pdf_to_folders(file_path, array_id)
+
+                # Debug: Exibir informações do arquivo e IDs
+                print(Fore.GREEN + f"Arquivo enviado: {uploaded_file.name}" + Style.RESET_ALL)
+                print(Fore.GREEN + f"IDs das pastas: {array_id}" + Style.RESET_ALL)
+
+                # Remover o arquivo temporário
+                os.remove(file_path)
+
+                # Retornar resultados como JSON
+                return JsonResponse({'results': results})
+            except Exception as e:
+                print(Fore.RED + f"Erro ao processar o arquivo: {str(e)}" + Style.RESET_ALL)
+                return HttpResponse(Fore.RED + "Erro ao processar o arquivo." + Style.RESET_ALL, status=500)
         else:
             return HttpResponse("Nenhum arquivo enviado.", status=400)
 
-        return render(request, 'learning_logs/subir_arquivo.html', {'array': array_id, 'file_url': file_url})
-    else:
-        return HttpResponse("Método inválido.", status=400)
+    return render(request, 'learning_logs/subir_arquivo.html', {'array': array_id})
+
 
 

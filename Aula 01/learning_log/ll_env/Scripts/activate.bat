ddfc7fdbe050e0c62ba1e09dbc98d8e1c899a921
Casasco <gabrielcasasco2@gmail.com>
2025-02-07 10:19:37 -0300

    Ultimas Alteracoes

--- Aula 01/learning_log/ll_env/Scripts/activate.bat
+++ Aula 01/learning_log/ll_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\gabri\Desktop\UploadContratoAutomatico\Aula 01\learning_log\ll_env
+set "VIRTUAL_ENV=C:\Users\Geral\Desktop\teste teste teste\UploadContratoAutomatico\Aula 01\learning_log\ll_env"
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -24,7 +24,8 @@ set PYTHONHOME=
 if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
-set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+set "VIRTUAL_ENV_PROMPT=(ll_env) "
 
 :END
 if defined _OLD_CODEPAGE (
